d48a7f25cf8ed0a2cd7dcaaae9ba9306beaa71ee
alinakyrianova <alina.kyrianova@gmail.com>
2023-06-28 16:19:36 +0200

    hw7.01/2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleryAdd = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a></li>`
+  )
+  .join("");
+galleryEl.innerHTML = galleryAdd;
 
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
 console.log(galleryItems);
+
